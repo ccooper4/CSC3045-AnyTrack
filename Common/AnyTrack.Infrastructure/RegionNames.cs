15a70e47b6447a555b6894591a91c023e023893a
40081529 <40081529@eeecs.qub.ac.uk>
2015-10-26 13:19:15 +0000

    Creating base shell view and adding regions

---
+++ Common/AnyTrack.Infrastructure/RegionNames.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyTrack.Infrastructure
+{
+    /// <summary>
+    /// Region name manager. Add new region name constants here. 
+    /// </summary>
+    public static class RegionNames
+    {
+        /// <summary>
+        /// The main region in the UI.
+        /// </summary>
+        public const string MainRegion = "MainRegion";
+
+        /// <summary>
+        /// The side region in the UI
+        /// </summary>
+        public const string SideRegion = "SideRegion";
+    }
+}
