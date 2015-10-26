5878e80ff9f022dfc673af832b5de9a279b12287
40081529 <40081529@eeecs.qub.ac.uk>
2015-10-26 20:29:30 +0000

    Initial Registration UI implementation

---
+++ Modules/AnyTrack.Accounting/Views/RegistrationViewModel.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnyTrack.Accounting.ServiceGateways;
+using Microsoft.Practices.Unity;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace AnyTrack.Accounting.Views
+{
+    /// <summary>
+    /// The view model for the registration page. 
+    /// </summary>
+    public class RegistrationViewModel : BindableBase
+    {
+        #region Fields 
+
+        /// <summary>
+        /// The region manager.
+        /// </summary>
+        private readonly IRegionManager regionManager;
+
+        /// <summary>
+        /// The specified email address.
+        /// </summary>
+        private string email;
+
+        /// <summary>
+        /// The specified first name.
+        /// </summary>
+        private string firstName;
+
+        /// <summary>
+        /// The specified last name. 
+        /// </summary>
+        private string lastName;
+
+        /// <summary>
+        /// The specified password.
+        /// </summary>
+        private string password;
+
+        /// <summary>
+        /// The specified confirmation password.
+        /// </summary>
+        private string confirmPassword;
+
+        /// <summary>
+        /// The specified user skills.
+        /// </summary>
+        private ObservableCollection<UserSkills> skills;
+
+        #endregion
+
+        /// <summary>
+        /// RegistrationViewModel constructor.
+        /// </summary>
+        /// <param name="regionManager">The region manager.</param>
+        public RegistrationViewModel(IRegionManager regionManager)
+        {
+            if (regionManager == null)
+            {
+                throw new ArgumentNullException("regionManager");
+            }
+
+            this.regionManager = regionManager;
+
+            RegisterUserCommand = new DelegateCommand(this.RegisterUser, this.CanRegister);
+        }
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets Email property.
+        /// </summary>
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                SetProperty(ref email, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets First name property.
+        /// </summary>
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            set
+            {
+                SetProperty(ref firstName, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets Last name property.
+        /// </summary>
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            set
+            {
+                SetProperty(ref lastName, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets Password property.
+        /// </summary>
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                SetProperty(ref password, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets Confirm Password property.
+        /// </summary>
+        public string ConfirmPassword
+        {
+            get
+            {
+                return confirmPassword;
+            }
+
+            set
+            {
+                SetProperty(ref confirmPassword, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the Skills property.
+        /// </summary>
+        public ObservableCollection<UserSkills> Skills
+        {
+            get
+            {
+                return skills;
+            }
+
+            set
+            {
+                SetProperty(ref skills, value);
+            }
+        } 
+
+        /// <summary>
+        /// Gets the command used to register a user. 
+        /// </summary>
+        public DelegateCommand RegisterUserCommand { get; private set; }
+        #endregion
+
+        #region Methods 
+
+        /// <summary>
+        /// Detects whether the registration can take place.
+        /// </summary>
+        /// <returns>Proceed with registration or not.</returns>
+        private bool CanRegister()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Perform registration.
+        /// </summary>
+        private void RegisterUser()
+        {
+            regionManager.RequestNavigate(Infrastructure.RegionNames.MainRegion, "Login");
+        }
+
+        #endregion 
+    }
+}
---
+++ Testing/Unit/Modules/AnyTrack.Accounting/Views/RegistrationViewModelTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnyTrack.Accounting.Views;
+using AnyTrack.Backend.Data;
+using AnyTrack.Infrastructure;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Prism.Regions;
+using Unit.Backend.AnyTrack.Backend.Data.EntityRepositoryTests;
+
+namespace Unit.Modules.AnyTrack.Accounting.Views.RegistrationViewModelTests
+{
+    #region Context
+
+    public class Context
+    {
+        public static IRegionManager regionManager;
+        public static RegistrationViewModel registrationViewModel;
+
+        [SetUp]
+        public void ContextSetup()
+        {
+            regionManager = Substitute.For<IRegionManager>();
+            registrationViewModel = new RegistrationViewModel(regionManager);
+        }
+    }
+
+    #endregion
+
+    #region Tests
+
+    public class RegistrationViewModelTests : Context
+    {
+        [Test]
+        [ExpectedException (typeof(ArgumentNullException))]
+        public void TestNullConstructor()
+        {
+            registrationViewModel = new RegistrationViewModel(null);
+        }
+
+        [Test]
+        public void TestCanRegister()
+        {
+            var result = registrationViewModel.Call<bool>("CanRegister");
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void TestRegisterUser()
+        {
+            //registrationViewModel.Call("RegisterUser");
+            //regionManager.Received().RequestNavigate(RegionNames.MainRegion, "Login");
+        }
+
+    }
+
+    #endregion
+}
