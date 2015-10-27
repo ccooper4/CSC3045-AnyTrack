0040f7558f181976314858e9eb79d262f14a7db9
Chris Cooper <ccooper09@qub.ac.uk>
2015-10-27 18:52:33 +0000

    T3- added unit test file for login

---
+++ Testing/Unit/Modules/AnyTrack.Accounting/Views/LoginViewModelTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnyTrack.Accounting.Views;
+using AnyTrack.Backend.Data;
+using AnyTrack.Infrastructure;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Prism.Regions;
+using Unit.Backend.AnyTrack.Backend.Data.EntityRepositoryTests;
+using AnyTrack.Accounting.ServiceGateways;
+using AnyTrack.Accounting.ServiceGateways.Models;
+
+namespace Unit.Modules.AnyTrack.Accounting.Views
+{
+    #region Context
+
+    public class Context
+    {
+        public static IRegionManager regionManager;
+        public static IAccountServiceGateway gateway;
+        public static LoginViewModel loginViewModel;
+
+        [SetUp]
+        public void ContextSetup()
+        {
+            regionManager = Substitute.For<IRegionManager>();
+            gateway = Substitute.For<IAccountServiceGateway>();
+            loginViewModel = new LoginViewModel(regionManager);
+        }
+    }
+
+    #endregion
+
+    #region Tests
+
+    public class LoginViewModelTests : Context
+    {
+        #region Constructor Tests
+
+        #endregion
+
+        #region CanLogin() Tests
+
+        [Test]
+        public void TestCanLogin()
+        {
+            var result = loginViewModel.Call<bool>("CanLogin");
+            result.Should().BeTrue();
+        }
+
+        #endregion
+        
+    }
+
+    #endregion
+}
