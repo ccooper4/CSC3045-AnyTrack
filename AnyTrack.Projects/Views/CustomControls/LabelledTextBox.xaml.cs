4753225f90e6d8b9bc7595531c79fa5de0993371
David Fee <dfee05@qub.ac.uk>
2015-11-21 15:26:36 +0000

    Added templating to Projects area, ie for creating new project so far

---
+++ AnyTrack.Projects/Views/CustomControls/LabelledTextBox.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnyTrack.Projects.Views.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for LabelledTextBox.xaml
+    /// </summary>
+    public partial class LabelledTextBox
+    {
+        /// <summary>
+        /// constructor for making a label
+        /// </summary>
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(string), typeof(LabelledTextBox), new FrameworkPropertyMetadata("Unnamed Label"));
+
+        /// <summary>
+        /// constructor for making a textbox
+        /// </summary>
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(LabelledTextBox), new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        /// <summary>
+        /// constructor for making a labeled textbox
+        /// </summary>
+        public LabelledTextBox()
+        {
+            InitializeComponent();
+            Root.DataContext = this;
+        }
+
+        /// <summary>
+        /// Gets or sets Label property.
+        /// </summary>
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets Text property.
+        /// </summary>
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+
+    /// <summary>
+    /// LayoutGroup for sharing scope
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed.")]
+    public partial class LayoutGroup : StackPanel
+    {
+        /// <summary>
+        /// sharing the scope for the layoutgroup
+        /// </summary>
+        public LayoutGroup()
+        {
+            Grid.SetIsSharedSizeScope(this, true);
+        }
+    }
+}
