4ec5826999e47db71b27283643d335310fb65789
Chris Cooper <ccooper09@qub.ac.uk>
2015-11-26 12:09:52 +0000

    Update for tasks

---
+++ Backend/AnyTrack.Backend/Service/Model/ServiceTask.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyTrack.Backend.Service.Model
+{
+    /// <summary>
+    /// Service task class
+    /// </summary>
+    public class ServiceTask
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Service task constructor
+        /// </summary>
+        public ServiceTask()
+        {
+            UpdatedHours = new List<ServiceUpdatedHours>();
+            TaskId = new Guid();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Gets or sets Task ID
+        /// </summary>
+        public Guid TaskId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the story this task is part of.
+        /// </summary>
+        public ServiceSprintStory SprintStory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Conditions Of Satisfaction
+        /// </summary>
+        public string ConditionsOfSatisfaction { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Summary
+        /// </summary>
+        public string Summary { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Summary
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Assignee
+        /// </summary>
+        public ServiceUser Assignee { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Tester
+        /// </summary>
+        public ServiceUser Tester { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Hours Remaining
+        /// </summary>
+        public double HoursRemaining { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether task is blocked or not
+        /// </summary>
+        public bool Blocked { get; set; }
+
+        /// <summary>
+        /// Gets or sets the task's updated hours
+        /// </summary>
+        public ICollection<ServiceUpdatedHours> UpdatedHours { get; set; }
+    }
+}
