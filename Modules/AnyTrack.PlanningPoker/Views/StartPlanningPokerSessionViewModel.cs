674444c2ddd9d284d4dcb431d030f6a183ccf4d2
Andrew Fletcher <afletcher04@qub.ac.uk>
2015-11-28 02:08:50 +0000

    T17 - Adding view and view model for sprint manager's create session view.

---
+++ Modules/AnyTrack.PlanningPoker/Views/StartPlanningPokerSessionViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnyTrack.Infrastructure;
+using AnyTrack.PlanningPoker.ServiceGateways;
+using Prism.Regions;
+
+namespace AnyTrack.PlanningPoker.Views
+{
+    /// <summary>
+    /// The view model that allows a scrum master to start a planning poker session.
+    /// </summary>
+    public class StartPlanningPokerSessionViewModel : ValidatedBindableBase, INavigationAware, IRegionMemberLifetime
+    {
+        #region Fields 
+
+        /// <summary>
+        /// The planning poker service gateway.
+        /// </summary>
+        private IPlanningPokerManagerServiceGateway serviceGateway; 
+
+        #endregion 
+
+        #region Constructor 
+
+        /// <summary>
+        /// Constructs a new instance of the start planning poker view model with the specified dependencies.
+        /// </summary>
+        /// <param name="gateway">The service gateway.</param>
+        public StartPlanningPokerSessionViewModel(IPlanningPokerManagerServiceGateway gateway)
+        {
+            if (gateway == null)
+            {
+                throw new ArgumentNullException("gateway");
+            }
+
+            this.serviceGateway = gateway;
+        }
+
+        #endregion 
+
+        #region Properties 
+
+        /// <summary>
+        /// Gets a value indicating whether or not this view/view model should be reused.
+        /// </summary>
+        public bool KeepAlive
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        #endregion 
+
+        #region Methods 
+
+        /// <summary>
+        /// Allows this view model to specify if it can be re-used as a view model.
+        /// </summary>
+        /// <param name="navigationContext">The current navigation context.</param>
+        /// <returns>A true or false value indicating if this view can be used as the new view.</returns>
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Allows this view model to run any custom logic when the region system navigates from it.
+        /// </summary>
+        /// <param name="navigationContext">The current navigation context.</param>
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Allows this view model to run any custom logic when the region system navigates to it.
+        /// </summary>
+        /// <param name="navigationContext">The current navigation context.</param>
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion 
+    }
+}
---
+++ Testing/Unit/Modules/AnyTrack.PlanningPoker/Views/StartPlanningPokerSessionViewModelTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+using NSubstitute;
+using AnyTrack.PlanningPoker.Views;
+using AnyTrack.PlanningPoker.ServiceGateways;
+using Prism.Regions;
+
+namespace Unit.Modules.AnyTrack.PlanningPoker.Views.StartPlanningPokerSessionViewModelTests
+{
+    #region Setup
+
+    public class Context
+    {
+        public static IPlanningPokerManagerServiceGateway serviceGateway;
+        public static StartPlanningPokerSessionViewModel vm;
+
+        [SetUp]
+        public void SetUp()
+        {
+            serviceGateway = Substitute.For<IPlanningPokerManagerServiceGateway>();
+            vm = new StartPlanningPokerSessionViewModel(serviceGateway);
+        }
+    }
+
+    #endregion 
+
+    #region Tests 
+
+    public class StartPlanningPokerSessionViewModelTests : Context
+    {
+        #region Constructor Tests 
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructWithNoService()
+        {
+            vm = new StartPlanningPokerSessionViewModel(null);
+        }
+
+        #endregion 
+
+        #region IsNavigationTarget(NavigationContext navigationContext) Tests 
+
+        [Test]
+        public void CallIsNavigationTarget()
+        {
+            var navService = Substitute.For<IRegionNavigationService>();
+            var navContext = new NavigationContext(navService, new Uri("Test", UriKind.Relative));
+
+            var res = vm.IsNavigationTarget(navContext);
+
+            res.Should().BeFalse();
+        }
+
+        #endregion 
+    }
+
+    #endregion 
+}
