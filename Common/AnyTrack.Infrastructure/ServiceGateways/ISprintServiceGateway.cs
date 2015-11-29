8a258ee2549e41fe4430983682abaf93bdde1ef2
Pamela Nelson <pnelson08@qub.ac.uk>
2015-11-29 14:12:27 +0000

    Pamela: Sprint gateways didn't get added to new location in dev. Adding now.

---
+++ Common/AnyTrack.Infrastructure/ServiceGateways/ISprintServiceGateway.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using AnyTrack.Infrastructure.BackendSprintService;
+
+namespace AnyTrack.Infrastructure.ServiceGateways
+{
+    /// <summary>
+    /// Outlines the Sprint Gateway.
+    /// </summary>
+    public interface ISprintServiceGateway
+    {
+        /// <summary>
+        /// Creates a sprint and adds it to the project.
+        /// </summary>
+        /// <param name="projectId">Id of the project to add the sprint to</param>
+        /// <param name="sprint">ServiceSprint entity</param>
+        void AddSprint(Guid projectId, ServiceSprint sprint);
+
+        /// <summary>
+        /// Edits an exiting sprint.
+        /// </summary>
+        /// <param name="sprintId">Id of the sprint to be edited</param>
+        /// <param name="updatedSprint">ServiceSprint entity containing changes</param>
+        void EditSprint(Guid sprintId, ServiceSprint updatedSprint);
+
+        /// <summary>
+        /// Gets all tasks for sprint
+        /// </summary>
+        /// <param name="sprintId">The sprint id</param>
+        /// <returns>A list of tasks</returns>
+        List<ServiceTask> GetAllTasksForSprint(Guid sprintId);
+    }
+}
---
+++ Common/AnyTrack.Infrastructure/ServiceGateways/SprintServiceGateway.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using AnyTrack.Infrastructure.BackendSprintService;
+
+namespace AnyTrack.Infrastructure.ServiceGateways
+{
+    /// <summary>
+    /// Provides a gateway to the Sprint Service
+    /// </summary>
+    public class SprintServiceGateway : ISprintServiceGateway
+    {
+        #region Fields
+        /// <summary>
+        /// The web client
+        /// </summary>
+        private readonly ISprintService client;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new project service gateway
+        /// </summary>
+        /// <param name="client">The web client</param>
+        public SprintServiceGateway(ISprintService client)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException("client");
+            }
+
+            this.client = client;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Creates a sprint and adds it to the project.
+        /// </summary>
+        /// <param name="projectId">Id of the project to add the sprint to</param>
+        /// <param name="sprint">ServiceSprint entity</param>
+        public void AddSprint(Guid projectId, ServiceSprint sprint)
+        {
+            client.AddSprint(projectId, sprint);
+        }
+
+        /// <summary>
+        /// Edits an exiting sprint.
+        /// </summary>
+        /// <param name="sprintId">Id of the sprint to be edited</param>
+        /// <param name="updatedSprint">ServiceSprint entity containing changes</param>
+        public void EditSprint(Guid sprintId, ServiceSprint updatedSprint)
+        {
+            client.EditSprint(sprintId, updatedSprint);
+        }
+
+        /// <summary>
+        /// Get all tasks for sprint
+        /// </summary>
+        /// <param name="sprintId">The sprint id</param>
+        /// <returns>Returns a list of tasks</returns>
+        public List<ServiceTask> GetAllTasksForSprint(Guid sprintId)
+        {
+            return new List<ServiceTask>(client.GetAllTasksForSprint(sprintId));
+        }
+
+        #endregion
+    }
+}
