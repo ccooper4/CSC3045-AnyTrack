64c54d56859bd6058238ea64456b8390d75feaa5
Chris Cooper <ccooper09@qub.ac.uk>
2015-12-02 00:57:36 +0000

    Update to factor in user estimates for planning poker

---
+++ Backend/AnyTrack.Backend/Service/Model/ServicePlanningPokerEstimate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnyTrack.Backend.Service.Model
+{
+    /// <summary>
+    /// Class for planning poker estimates
+    /// </summary>
+    public class ServicePlanningPokerEstimate
+    {
+        /// <summary>
+        /// Gets or sets the session id. 
+        /// </summary>
+        public Guid SessionID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's estimate
+        /// </summary>
+        public double Estimate { get; set; }
+    }
+}
